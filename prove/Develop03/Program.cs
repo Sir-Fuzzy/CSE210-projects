99368074cbd8bf33ef2efe6b9745760c69c82217
Ian Black <eeian09@gmail.com>
2024-05-11 20:08:20 -0600

    Program class finish v1.0

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference reference = new Reference("2 Nephi",2,4);
+        Scripture scripture = new Scripture(reference, "And thou hast beheld in thy youth his glory; wherefore, thou art blessed even as they unto whom he shall minister in the flesh; for the Spirit is the same, yesterday, today, and forever. And the way is prepared from the fall of man, and salvation is free.");
+        Random random = new Random();
+        string userInput = "";
+        
+        
+        while (scripture.IsCompletelyHidden() == false || userInput == "quit")
+        {
+            scripture.GetDisplayText();
+            userInput = Console.ReadLine();
+            int randomNumber = random.Next(1,5);
+            scripture.HideRandomWords(randomNumber);
+            Console.Clear();
+        }
     }
 }
