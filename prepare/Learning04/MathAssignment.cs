c864da31a8eb8e18bdaf62560e07d2f93b04e9f3
Ian Black <167881822+Sir-Fuzzy@users.noreply.github.com>
2024-05-14 11:00:20 -0600

    Test ... Final?

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,18 @@
+using System
+
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string name, string topic, string textbookSelection, string problems) : base(name, topic)
+    {
+        _textbookSection = textbookSelection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return "Section " + _textbookSection + " #(s)" + problems
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
-    }
+        Assignment assignment = new Assignment("James Smith", "School");
+        Console.WriteLine(assignment.GetSummary());
+
+
+        MathAssignment mathAssignment = new MathAssignment("John Smith","Math","7.2"."54-86");
+        Console.WriteLine(mathAssignment.GetHomeworkList);
+
+
+        WritingAssignment writingAssignment = new WritingAssignment("Jim Smith","Reading","Huck Finn");
+        Console.WriteLine(writingAssignment.GetWritingAssignment());
+    }    
 }
