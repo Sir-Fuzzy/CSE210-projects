82178fef3b8cfa42ee76732fc2ffeeda7b1e19fd
Ian Black <eeian09@gmail.com>
2024-05-20 22:16:21 -0600

    Finished the prepare activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Orange",2);
+
+        // Console.WriteLine($"{square.GetArea()}, {square.GetColor()}");
+
+        Rectangle rectangle = new Rectangle("Green", 2,4);
+
+        // Console.WriteLine($"{rectangle.GetArea()}, {rectangle.GetColor()}");
+
+        Circle circle = new Circle("Pink", 1);
+
+        // Console.WriteLine($"{circle.GetArea()}, {circle.GetColor()}");
+
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+        shapes.Add(square);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape.GetArea()}, {shape.GetColor()}");
+        }
+
     }
 }
